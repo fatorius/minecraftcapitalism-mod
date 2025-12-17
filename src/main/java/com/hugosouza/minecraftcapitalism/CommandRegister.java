61ce41e8ffb2aadc2b1a3db47c7a1caac8a9f23c
HugoSouza <hugo.souza0610@gmail.com>
2025-12-16 23:09:06 -0300

    Unifica comandos

--- src/main/java/com/hugosouza/minecraftcapitalism/CommandRegister.java
+++ src/main/java/com/hugosouza/minecraftcapitalism/CommandRegister.java
@@ -15,10 +15,12 @@ import net.minecraft.commands.arguments.item.ItemArgument;
 public final class CommandRegister {
     public static void register(CommandDispatcher<CommandSourceStack> dispatcher, CommandBuildContext commandContext) {
         dispatcher.register(
-            Commands.literal("saldo")
+            Commands.literal("capitalismo")
+
+            // /saldo
+            .then(Commands.literal("saldo")
                 .requires(src -> src.hasPermission(0))
 
-                // /saldo
                 .executes(ConsultSaldo::run)
 
                 // /saldo set <player> <valor>
@@ -44,105 +46,121 @@ public final class CommandRegister {
                         )
                     )
                 )
-        );
+            )
 
-
-        // /pix <player> <value>
-        dispatcher.register(
-            Commands.literal("pix")
-                .then(
-                    Commands.argument("player", EntityArgument.player())
+            // /pix <player> <value>
+            .then(
+                dispatcher.register(
+                    Commands.literal("pix")
                     .then(
-                        Commands.argument("valor", IntegerArgumentType.integer(0))
-                        .executes(SendPix::run)
-                    )
-                )
-        );
-
-        // /extrato
-        dispatcher.register(
-            Commands.literal("extrato")
-
-                // /extrato (página 1 por padrão)
-                .executes(ctx -> ConsultExtrato.run(ctx, 1))
-
-                // /extrato <page>
-                .then(
-                    Commands.argument("page", IntegerArgumentType.integer(1))
-                    .executes(ctx -> {
-                        int page = IntegerArgumentType.getInteger(ctx, "page");
-                        return ConsultExtrato.run(ctx, page);
-                    })
-                )
-        );
-
-        // /cobrar <player> <value>
-        dispatcher.register(
-            Commands.literal("cobrar")
-                .then(
-                    Commands.argument("player", EntityArgument.player())
-                    .then(
-                        Commands.argument("valor", IntegerArgumentType.integer(1))
-                        .executes(Cobrar::criar)
-                    )
-                )
-        );
-
-        // /pagarcobranca
-        dispatcher.register(
-            Commands.literal("pagarcobranca")
-                .then(
-                    Commands.argument("id", IntegerArgumentType.integer(1))
-                    .executes(Cobrar::pagar)
-                )
-        );
-
-        // /recusarcobranca
-        dispatcher.register(
-            Commands.literal("recusarcobranca")
-                .then(
-                    Commands.argument("id", IntegerArgumentType.integer(1))
-                    .executes(Cobrar::recusar)
-                )
-        );
-
-        // /tigrinho
-        dispatcher.register(
-            Commands.literal("tigrinho")
-                .then(
-                    Commands.argument("value", IntegerArgumentType.integer(1))
-                    .executes(Tigrinho::apostar)
-                )
-        );
-
-        // /sell <item> <quantidade> <valor>
-        dispatcher.register(
-            Commands.literal("sell")
-                .then(
-                    Commands.argument("item", ItemArgument.item(commandContext))
-                    .then(
-                        Commands.argument("quantidade", IntegerArgumentType.integer(1))
+                        Commands.argument("player", EntityArgument.player())
                         .then(
-                            Commands.argument("valor", IntegerArgumentType.integer(1))
-                            .executes(Mercado::sell)
+                            Commands.argument("valor", IntegerArgumentType.integer(0))
+                            .executes(SendPix::run)
                         )
                     )
                 )
-        );
+            )
 
-        // /mercado
-        dispatcher.register(
-            Commands.literal("mercado")
-                .then(
-                    Commands.argument("item", ItemArgument.item(commandContext))
+            // /extrato
+            .then(
+                dispatcher.register(
+                    Commands.literal("extrato")
+
+                    // página 1 por padrão
+                    .executes(ctx -> ConsultExtrato.run(ctx, 1))
+                    
                     .then(
-                        Commands.argument("quantidade", IntegerArgumentType.integer(1))
+                        // /extrato <page>
+                        Commands.argument("page", IntegerArgumentType.integer(1))
+                        .executes(ctx -> {
+                            int page = IntegerArgumentType.getInteger(ctx, "page");
+                            return ConsultExtrato.run(ctx, page);
+                        })
+                    )
+                )
+            )
+
+            // /cobrar <player> <value>
+            .then(
+                dispatcher.register(
+                    Commands.literal("cobrar")
+                    .then(
+                        Commands.argument("player", EntityArgument.player())
                         .then(
                             Commands.argument("valor", IntegerArgumentType.integer(1))
-                            .executes(Mercado::sell)
+                            .executes(Cobrar::criar)
                         )
                     )
                 )
+            )
+
+            // /pagarcobranca
+            .then(
+                dispatcher.register(
+                    Commands.literal("pagarcobranca")
+                    .then(
+                        Commands.argument("id", IntegerArgumentType.integer(1))
+                        .executes(Cobrar::pagar)
+                    )
+                )
+            )
+            
+            // /recusarcobranca
+            .then(
+                dispatcher.register(
+                    Commands.literal("recusarcobranca")
+                    .then(
+                        Commands.argument("id", IntegerArgumentType.integer(1))
+                        .executes(Cobrar::recusar)
+                    )
+                )
+            )
+            
+            // /tigrinho
+            .then(
+                dispatcher.register(
+                    Commands.literal("tigrinho")
+                    .then(
+                        Commands.argument("value", IntegerArgumentType.integer(1))
+                        .executes(Tigrinho::apostar)
+                    )
+                )
+            )
+            
+            // /sell <item> <quantidade> <valor>
+            .then(
+                dispatcher.register(
+                    Commands.literal("sell")
+                    .then(
+                        Commands.argument("item", ItemArgument.item(commandContext))
+                        .then(
+                            Commands.argument("quantidade", IntegerArgumentType.integer(1))
+                            .then(
+                                Commands.argument("valor", IntegerArgumentType.integer(1))
+                                .executes(Mercado::sell)
+                            )
+                        )
+                    )
+                )
+            )
+            
+            // /mercado
+            .then(
+                dispatcher.register(
+                    Commands.literal("mercado")
+                    .then(
+                        Commands.argument("item", ItemArgument.item(commandContext))
+                        .then(
+                            Commands.argument("quantidade", IntegerArgumentType.integer(1))
+                            .then(
+                                Commands.argument("valor", IntegerArgumentType.integer(1))
+                                .executes(Mercado::sell)
+                            )
+                        )
+                    )
+                );
+            )
         );
     }
 }
